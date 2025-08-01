02ac772f4d86a9fc79d224418c589713e3da4909
pruthviraj-gaikwad <pruthviraj162004@gmail.com>
2025-08-01 21:06:27 +0530

    no change

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import HeroSection from '../components/HeroSection'
 import FeaturedSection from '../components/FeaturedSection'
 import TrailersSection from '../components/TrailersSection'
-import App from '../components/app'
 
 const Home = () => {
   return (
